30e05477ff2a47d70552fc890eee4ed7438f2748
Brad Copenhaver and Kevin Finley <kevin@>
2017-01-12 10:34:12 -0800

    add delete function to our post page

--- app/routes/post.js
+++ app/routes/post.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
+  },
+  actions: {
+    delete(post) {
+      if (confirm('Are your sure you want to delete')) {
+        post.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
   }
 });
